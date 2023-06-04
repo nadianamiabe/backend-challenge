0590e3abbff3be13a49075ab28e9f5644eccb1b6
nadianamiabe <abe.nami0510@gmail.com>
2023-06-04 17:01:16 -0300

    git commit -m "fix migrations"

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,4 +1,4 @@
-create table `user`(
+create table application_user(
     id varchar(36) primary key,
     email varchar(50) not null unique,
     document_id varchar(50) not null unique,
@@ -11,25 +11,25 @@ create table role(
     name varchar(50) primary key
 );
 
-create table user_roles(
-    user_id varchar(36) not null,
-    role_id varchar(36) not null,
-    primary key (user_id, role_id),
-    foreign key (user_id) references `user` (id),
-    foreign key (role_id) references role (name)
+create table application_user_roles(
+    application_user_id varchar(36) not null,
+    roles_name varchar(50) not null,
+    primary key (application_user_id, roles_name),
+    foreign key (application_user_id) references application_user (id),
+    foreign key (roles_name) references role (name)
 );
 
 create table wallet(
     id varchar(36) primary key,
     balance decimal(10, 2) not null,
-    user_id varchar(36) not null,
-    constraint fk_user foreign key (user_id) references `user` (id)
+    application_user_id varchar(36) not null,
+    constraint fk_user foreign key (application_user_id) references application_user (id)
 );
 
 create table `transaction`(
     id varchar(36) not null,
     amount decimal(10, 2) not null,
-    user_id varchar(36) not null,
+    application_user_id varchar(36) not null,
     sender_wallet_id varchar(36) not null,
     receiver_wallet_id varchar(36) not null,
     created_date datetime not null,
--- src/main/resources/db/migration/V3__insert_users_and_relationships.sql
+++ src/main/resources/db/migration/V3__insert_users_and_relationships.sql
@@ -1,19 +1,19 @@
-insert into `user`(id, email, document_id, full_name, password, created_date)
-values ('85c0cc96-0dd4-4d61-827f-4b05c0885f49', 'maria@email.com', '17336696040', 'Maria da Silva', '123', '2023-05-24 08:57:00.0');
+insert into application_user(id, email, document_id, full_name, password, created_date)
+values ('85c0cc96-0dd4-4d61-827f-4b05c0885f49', 'maria@email.com', '17336696040', 'Maria da Silva', '$2a$12$zhKjIsObIb.7hyge3vDJj.NhK1XmD9jj2iKSkemtJsEdWE20RSLq2', '2023-05-24 08:57:00.0');
 
-insert into `user`(id, email, document_id, full_name, password, created_date)
-values ('2cf4ff1c-0879-4a03-8ab1-8139ed277322', 'joao@email.com', '43990029053', 'Joao da Silva', '123', '2023-05-24 08:57:00.0');
+insert into application_user(id, email, document_id, full_name, password, created_date)
+values ('2cf4ff1c-0879-4a03-8ab1-8139ed277322', 'joao@email.com', '43990029053', 'Joao da Silva', '$2a$12$GgNICYtcF8uS0SnKQUEb0uaj5eXjDser4ssBvaqF7n7frSCVcsiNC', '2023-05-24 08:57:00.0');
 
 
-insert into user_roles(user_id, role_id)
+insert into application_user_roles(application_user_id, roles_name)
 values ('85c0cc96-0dd4-4d61-827f-4b05c0885f49', 'COMMON_USER');
 
-insert into user_roles(user_id, role_id)
+insert into application_user_roles(application_user_id, roles_name)
 values ('2cf4ff1c-0879-4a03-8ab1-8139ed277322', 'SHOPKEEPER_USER');
 
 
-insert into wallet(id, balance, user_id)
+insert into wallet(id, balance, application_user_id)
 values ('ae2acfef-4ba4-405b-bc7f-9c94b664d86b', '1000.00', '85c0cc96-0dd4-4d61-827f-4b05c0885f49');
 
-insert into wallet(id, balance, user_id)
+insert into wallet(id, balance, application_user_id)
 values ('548658ed-6365-4336-8367-ea0f0be78459', '2000.00', '2cf4ff1c-0879-4a03-8ab1-8139ed277322');
